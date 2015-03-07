3d7584187f5be29825bc0949c64b146cfa35bf9f
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2015-03-07 07:17:15 +0100

    * helm-bbdb.el: Fix version of bbdb.

--- helm-bbdb.el
+++ helm-bbdb.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2012 ~ 2014 Thierry Volpiatto <thierry.volpiatto@gmail.com>
 
 ;; Version: 1.6.8
-;; Package-Requires: ((helm "1.5") (bbdb "0"))
+;; Package-Requires: ((helm "1.5") (bbdb "3.1.2"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
